05ebcd74c1f94d5fe7d5c09158c3b37d14d25aa6
emanespitia <eespiti@jacksonville.edu>
2024-10-02 17:40:24 -0400

    Finished code

--- Javascript.js
+++ Javascript.js
@@ -8,36 +8,74 @@ class car
         this.price = price
     }
 
-    depretiation() 
+    calculatedPrice() 
     {
-        dp = this.price - 500(2024 - this.year)
+        let dp = this.price - 500*(2024 - this.year)
         if (dp <= 0) 
         {
             return 0
         }
         else 
         {
-            return 0
+            return dp
         }
     }
 }
 
 class carManager 
 {
+    //create the array for cars
     constructor()
     {
-        this.cars=[];
+        this.cars=[]
     }
 
-    addCar()
+    //add car to array/inventory and to list
+    addCar(car)
     {
-        
+        this.cars.push(car)
+        this.displayCars()
     }
+
 
     displayCars()
     {
+        const list = document.getElementById('list')
+        list.innerHTML=''
+        //loop for all the car information to be displayed
+        this.cars.forEach(car =>{
+            const li = document.createElement('li')
+            li.textContent = (`${car.name} ${car.model} ${car.year} $${car.price}`)
+            list.appendChild(li)
+        })
 
     }
 
-
+    //display total price of all cars
+    showTotalPrice()
+    {
+        let sum = this.cars.reduce((total,car) => total + car.calculatedPrice(),0)
+        return sum;
+    }
 }
+
+//begin initializations
+const carManager1 = new carManager()
+
+document.getElementById('carSystem').addEventListener('submit', function(event){
+    event.preventDefault()
+    const name = document.getElementById('name').value
+    const model = document.getElementById('model').value
+    const year = parseInt(document.getElementById('year').value)
+    const price = parseFloat(document.getElementById('price').value)
+
+    const car1 = new car(name, model, year, price)
+
+    carManager1.addCar(car1)
+
+    document.getElementById('carSystem').reset()
+})
+
+document.getElementById('total').addEventListener('click', function(){
+    alert(carManager1.showTotalPrice())
+})
